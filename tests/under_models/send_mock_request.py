2ce5d7d95e37b4de1cc0990363f2716a874f99ce
gc-fu <guancheng.fu@intel.com>
2023-10-17 06:13:56 +0000

    add multiple requests test

--- tests/under_models/send_mock_request.py
+++ tests/under_models/send_mock_request.py
@@ -47,6 +47,31 @@ class UglyAsyncLLMEngine(LLMEngine):
         # TODO: change this to real one
         return RequestOutput(request_id=request_id, prompt="", prompt_token_ids=[1, 3087, 8970, 338, 263], outputs=[], finished=False)
 
+    async def step_async_multiple(self) -> List[RequestOutput]:
+        seq_group_metadata_lists = []
+        request_id_0= "cmpl-81e2b9767b5b47bca7e649482698d385"
+        seq_data_0 = {0: SequenceData(prompt_token_ids=[1, 3087, 8970, 338, 263])}
+        sampling_params_0 = SamplingParams(n=1, best_of=1, presence_penalty=0.0, frequency_penalty=0.0, temperature=0.0, top_p=1.0, top_k=-1, use_beam_search=False, length_penalty=1.0, early_stopping=False, stop=[], ignore_eos=False, max_tokens=7, logprobs=None, skip_special_tokens=True)
+
+        seq_group_metadata_lists.append(SequenceGroupMetadata(request_id_0, True, seq_data_0, sampling_params_0, {}))
+
+        request_id_1 = "cmpl-81e2b9767b5b47bca7e649482698d385"
+        seq_data_1 = {1: SequenceData(prompt_token_ids=[1, 3087, 8970, 338, 263])}
+        sampling_params_1 = SamplingParams(n=1, best_of=1, presence_penalty=0.0, frequency_penalty=0.0, temperature=0.0, top_p=1.0, top_k=-1, use_beam_search=False, length_penalty=1.0, early_stopping=False, stop=[], ignore_eos=False, max_tokens=7, logprobs=None, skip_special_tokens=True)
+
+        seq_group_metadata_lists.append(SequenceGroupMetadata(request_id_1, True, seq_data_1, sampling_params_1, {}))
+
+        output = await self._run_workers_async(
+            "execute_model",
+            seq_group_metadata_list=seq_group_metadata_lists,
+            blocks_to_swap_in={},
+            blocks_to_swap_out={},
+            blocks_to_copy={},
+        )
+
+        # TODO: change this to real one
+        return RequestOutput(request_id=request_id_0, prompt="", prompt_token_ids=[1, 3087, 8970, 338, 263], outputs=[], finished=False)
+
 
     async def _run_workers_async(
         self,
@@ -82,7 +107,7 @@ setattr(AsyncLLMEngine, "_engine_class", UglyAsyncLLMEngine)
 
 
 @pytest.mark.asyncio
-async def test_model_execution():
+async def test_model_execution_single_request():
     # Let's build an engine_args    
     engine_args = AsyncEngineArgs(model='/models/vicuna-7b/', tokenizer='/models/vicuna-7b/', tokenizer_mode='auto', trust_remote_code=False, download_dir=None, load_format='auto', dtype='auto', seed=0, max_model_len=None, worker_use_ray=False, pipeline_parallel_size=1, tensor_parallel_size=1, block_size=16, swap_space=16, gpu_memory_utilization=0.9, max_num_batched_tokens=None, max_num_seqs=256, disable_log_stats=False, revision=None, tokenizer_revision=None, quantization=None, engine_use_ray=False, disable_log_requests=True, max_log_len=None)
     # Start the engine
@@ -91,5 +116,9 @@ async def test_model_execution():
     engine.start_background_loop()
     await asyncio.sleep(2)
     await engine.engine.step_async()
+    # Now let's try something difficult
+    await engine.engine.step_async_multiple()
+
+
 
 
